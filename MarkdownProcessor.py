034ec698a6a8e44e0405a27c712b9a50c5c1126d
michaelmondoro <michaelmondoro@gmail.com>
2024-08-13 23:52:17 -0400

    processing power

--- MarkdownProcessor.py
+++ MarkdownProcessor.py
@@ -1,5 +1,6 @@
-class MarkdownProcessor:
+import re
 
+class MarkdownProcessor:
     def __init__(self):
         self.data = None
         self.chunks = []
@@ -9,3 +10,59 @@ class MarkdownProcessor:
             data = f.read()
         self.chunks = data.split("\n\n")
         self.data = data
+    
+    def process_bold(self, line):
+        bolds = re.findall("\*\*.*\*\*", line)
+        for bold in bolds:
+            tag = bold.replace("**", "<b>").replace("**", "</b>")
+            line = line.replace(bold, tag)
+        return line 
+    
+    def process_strong(self, line):
+        strongs = re.findall("__.*__", line)
+        for strong in strongs:
+            tag = strong.replace("__", "<strong>").replace("__", "</strong>")
+            line = line.replace(strong, tag)
+        return line 
+
+    def process_links(self, line):
+        links = re.findall("\[.*\]\(.*\)", line)
+        for link in links:
+            url = re.findall("\(.*\)", link)[0].strip("(").strip(")")
+            txt = re.findall("\[.*\]", link)[0].strip("[").strip("]")
+            line = line.replace(link, f"<a href='{url}'>{txt}</a>")
+        return line
+    
+    def convert_line(self, line):
+        line = line.strip()
+        if line.startswith("### "):
+            return line.replace("### ", "<h3>") + "</h3>"
+        if line.startswith("## "):
+            return line.replace("## ", "<h2>") + "</h2>"
+        if line.startswith("# "):
+            return line.replace("# ", "<h1>") + "</h1>"
+        
+        if line.startswith("```"):
+            return line.replace("```", "<code>").replace("```", "</code>")
+        if line.startswith("`"):
+            return line.replace("`", "<code>").replace("`", "</code>")
+        
+        # Default normal text
+        line = self.process_bold(line)
+        line = self.process_strong(line)
+        line = self.process_links(line)
+        return f"<p>{line}</p>"
+
+    def convert(self):
+        data = ""
+        for chunk in self.chunks:
+             data += self.convert_line(chunk) + "\n"
+        return data
+
+if __name__=="__main__":
+    proc = MarkdownProcessor()
+    proc.load('README.md')
+    # print(proc.convert())
+    print(proc.chunks)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MarkdownProcessor
+
 Convert markdown to html
+
+### test data
+again __hello__???
+
+`hello world`
+
+```
+print('hi')
+
+```
+
+[Link text Here](https://link-url-here.org)
+[aweomse website](https://www.google.com)
